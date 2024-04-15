df44f10079c85c046458931921bfa290c7da9d33
I_kushnir <kushnir_iv@kness.energy>
2024-04-15 16:59:29 +0300

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import re
 from telnetlib import EC
 from time import sleep
 from wsgiref import headers
+import json
 
 import requests
 from bs4 import BeautifulSoup
@@ -64,7 +66,7 @@ def parse_one_property(url):
         title = title_container.text.strip()
     else:
         title = "Назву не вдалося знайти"
-    print(title)
+
     address_container = soup.select_one(
         "#overview > div.row.property-tagline > div.d-none.d-sm-block.house-info > div > div.col.text-left.pl-0 div.d-flex.mt-1 h2.pt-1"
     )
@@ -74,25 +76,41 @@ def parse_one_property(url):
 
     region_parts = address_parts[1].strip().split()
     region = ' '.join(region_parts[-2:])
-    print(address)
-    print(region)
+
     price_container = soup.select_one(
         "#overview > div.row.property-tagline > div.d-none.d-sm-block.house-info > div > div.price-container > div.price.text-right"
     )
 
     if price_container:
         price = price_container.text.strip()
+        price = re.sub(r'[^\d.,]', '', price)
     else:
         price = "Ціну не вдалося знайти"
 
     print(price)
+    area = soup.select_one(
+        "#overview > div.grid_3 > div.col-lg-12.description > div:nth-child(6) > div:nth-child(1) > div.carac-value > span")
+    area_string = area.text.strip()
+    area = area_string.split()[0]
+
+    # rooms = soup.select_one("#overview > div.grid_3 > div.col-lg-12.description > div.row.teaser > div.col-lg-3.col-sm-6.cac")
+    # rooms_text = rooms.text.strip()
+    # rooms = ''.join(filter(str.isdigit, rooms_text))
 
     description_element = soup.find('div', class_='property-description')
+    try:
+        description_text = description_element.find('div', itemprop='description').get_text(strip=True)
+    except AttributeError as e:
+        print("Помилка при отриманні опису:", e)
+        description_text = None
 
-    description_text = description_element.find('div', itemprop='description').get_text(strip=True)
-
-    print(description_text)
-
+    meta_tags = soup.find_all('meta')
+    print(meta_tags)
+    for tag in meta_tags:
+        if tag.get('name') == 'last-modified':
+            last_modified = tag.get('content')
+            print('Дата останнього оновлення:', last_modified)
+            break
 
     try:
         driver = webdriver.Chrome(options=options)
@@ -183,15 +201,26 @@ def parse_one_property(url):
     # gallery > div.image-wrapper > div.wrap.activateNextArrow > div.logo
     except Exception as e:
         print("Помилка:", e)
-    finally:
-        driver.quit()
+    data = {
+        "link": url,
+        "title": title,
+        "address": address,
+        "region": region,
+        "price": price,
+        "area": area,
+        # "rooms": rooms,
+        "description": description_text,
+        "images": link_list
 
-    return (response.status_code)
+    }
+    print(data)
+    return json.dumps(data, ensure_ascii=False)
 
 
 def main():
     links = get_links()
-    parse_page(links)
+    parsed_data = [parse_one_property(url) for url in links[:1]]
+    print(parsed_data)
 
 
 if __name__ == '__main__':
